65fb09ef4d6d10e4d4d48e9265445695d17b51df
Kailar0000 <kailar0000@gmail.com>
2022-12-05 17:21:35 +0300

    Task1b

---
+++ task1b/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int max1 = 0;
+int max2 = 0;
+
+while(a>0)
+{
+    if(a>max1)
+    {
+        max2 = max1;
+        max1 = a;
+    }
+    else
+    {
+        if(a>max2)
+        max2 = a;
+    }
+    Console.Write("Введите число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write(max2);

b89f0207c4e5f6cf572f10ae5c80048cccf3e097
Kailar0000 <kailar0000@gmail.com>
2022-12-05 17:04:38 +0300

    Task13

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i1 = 0;
+int i2 = 0;
+int i3 = 0;
+int x = 0;
+
+while(a>0)
+{
+    x = x+1;
+    i3 = i2;
+    i2 = i1;
+    i1 = a%10;
+    a = a/10;
+}
+if(x>2)
+    Console.WriteLine(i3);
+else
+    Console.WriteLine("Третьей цифры нет");

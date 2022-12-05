04c299efe07df0c22ea897cf06880ce2887db0b5
Kailar0000 <kailar0000@gmail.com>
2022-12-05 16:38:41 +0300

    Task10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a/100>0 && a/100<10)
+{
+a = a%100;
+a = a/10;
+Console.WriteLine(a);
+}
+else
+Console.WriteLine("Error");

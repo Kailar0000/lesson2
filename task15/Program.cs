15b5b56773ad636d4218e131e1d70d57adc92e8b
Kailar0000 <kailar0000@gmail.com>
2022-12-05 17:13:17 +0300

    Task15

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a == 6 || a == 7)
+    Console.WriteLine("Да");
+else if(a>7)
+    Console.WriteLine("Error");
+else
+    Console.WriteLine("Нет");
